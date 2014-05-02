71c372cfa432c3e7170fc7c67d9878bd41ed60f9
Will Cosgrove <will@willcosgrove.com>
2014-05-02 18:44:12 -0500

    Updated rake job type to use bin/*

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,8 @@
 
 # Learn more: http://github.com/javan/whenever
 
+job_type :rake, "cd :path && :environment_variable=:environment bin/rake :task --silent :output"
+
 
 every 2.minutes do
   rake "dnsimple:update"
